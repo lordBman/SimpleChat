3b2246ce9b2e96aa7d81f8aef037147e099a0e96
Bsoft Limited <63307009+BsoftLimited@users.noreply.github.com>
2024-06-05 11:36:19 +0100

    index.ts - added loading component to index

--- pages/conponents/index.ts
+++ pages/conponents/index.ts
@@ -1,9 +1,9 @@
 import { FriendResultView, FriendView } from "./friends-view";
-import CircleLoading from "./loading";
+import CircleLoading, { Loading }from "./loading";
 import Menu from "./menu";
 import ChatView from "./chat";
 import BottomNavigation from "./bottom-navigation";
 import BackButton from "./back-button";
 import MobileHeader from "./mobile-header";
 
-export { FriendResultView, FriendView, CircleLoading, Menu, ChatView, BottomNavigation, BackButton, MobileHeader }
+export { FriendResultView, FriendView, CircleLoading, Loading, Menu, ChatView, BottomNavigation, BackButton, MobileHeader }

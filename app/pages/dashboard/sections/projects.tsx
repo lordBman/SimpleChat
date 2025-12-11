a43ff781d8072e3c26b7917bd64d51baa768e8b7
lordBman <okelekelenobel@gmail.com>
2025-12-11 05:46:30 +0100

    working on project section

--- app/pages/dashboard/sections/projects.tsx
+++ app/pages/dashboard/sections/projects.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useAppContext } from "../../providers/app-provider";
+import { usePageContext } from "../../providers/page-provider";
 
-const Projects = () =>{
+const rootStyle: React.CSSProperties = {
+    padding: "20px"
+}
+
+const projectListStyle: React.CSSProperties = {
+
+}
+
+const ProjectsSection = () =>{
+    const { user } = useAppContext();
+    const { pageState } = usePageContext();
+
     return (
-        <div>projects</div>
+        <div style={rootStyle}>
+            <h2>Projects</h2>
+            <div style={projectListStyle}>
+                
+            </div>
+        </div>
     );
 }
 
-export default Projects;
+export default ProjectsSection;
